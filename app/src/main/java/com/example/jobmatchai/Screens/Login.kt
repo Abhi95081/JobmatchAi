71f721111d6c7bbe2b206df85d9c4e5d8445e4cb
Abhi95081 <abhirou7@gmail.com>
2025-03-15 21:08:05 +0530

    Register And Login Screen Commit

--- app/src/main/java/com/example/jobmatchai/Screens/Login.kt
+++ app/src/main/java/com/example/jobmatchai/Screens/Login.kt
@@ -1,6 +1,9 @@
 package com.example.jobmatchai.Screens
 
 import android.widget.Toast
+import androidx.compose.animation.*
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
@@ -10,6 +13,7 @@ import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
@@ -21,93 +25,129 @@ import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import com.example.jobmatchai.Navigation.Routes
 
-
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun Login(navHostController: NavHostController) {
-
-
     var email by remember { mutableStateOf("") }
     var password by remember { mutableStateOf("") }
+    var passwordVisible by remember { mutableStateOf(false) }
+    var isVisible by remember { mutableStateOf(false) }
     val context = LocalContext.current
 
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(24.dp),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
+    LaunchedEffect(Unit) {
+        isVisible = true
+    }
+
+    AnimatedVisibility(
+        visible = isVisible,
+        enter = fadeIn(animationSpec = tween(1000)) + slideInVertically(initialOffsetY = { it / 2 }),
+        exit = fadeOut(animationSpec = tween(500))
     ) {
-        Text(
-            text = "Login",
-            style = TextStyle(fontWeight = FontWeight.ExtraBold, fontSize = 24.sp)
-        )
-
-        Spacer(modifier = Modifier.height(50.dp))
-
-        OutlinedTextField(
-            value = email,
-            onValueChange = { email = it },
-            label = { Text(text = "Email") },
-            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email),
-            singleLine = true,
-            modifier = Modifier.fillMaxWidth()
-        )
-
-        var passwordVisible by remember { mutableStateOf(false) }
-
-        OutlinedTextField(
-            value = password,
-            onValueChange = { password = it },
-            label = { Text(text = "Password") },
-            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
-            visualTransformation = if (passwordVisible) VisualTransformation.None else PasswordVisualTransformation(),
-            singleLine = true,
-            trailingIcon = {
-                val image = if (passwordVisible) Icons.Filled.Visibility else Icons.Filled.VisibilityOff
-                IconButton(onClick = { passwordVisible = !passwordVisible }) {
-                    Icon(imageVector = image, contentDescription = "Toggle password visibility")
-                }
-            },
-            modifier = Modifier.fillMaxWidth()
-        )
-
-
-        Spacer(modifier = Modifier.height(30.dp))
-
-        ElevatedButton(
-            onClick = {
-                if (email.isEmpty() || password.isEmpty()) {
-                    Toast.makeText(context, "Please provide all fields", Toast.LENGTH_SHORT).show()
-                } else {
-                    navHostController.navigate(Routes.BottomNav.routes) {
-                        popUpTo(Routes.Login.routes) { inclusive = true } // Removes Login screen from back stack
-                        launchSingleTop = true
-                    }
-                }
-            }
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(Color(0xFFBBDEFB))
+                .padding(24.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
         ) {
             Text(
                 text = "Login",
-                style = TextStyle(fontWeight = FontWeight.ExtraBold, fontSize = 20.sp),
-                modifier = Modifier.padding(vertical = 6.dp)
+                style = TextStyle(fontWeight = FontWeight.ExtraBold, fontSize = 24.sp)
             )
-        }
 
+            Spacer(modifier = Modifier.height(50.dp))
 
-        TextButton(
-            onClick = {
-                navHostController.navigate(Routes.Register.routes) {
-                    popUpTo(navHostController.graph.startDestinationId) {
-                        inclusive = true
+            OutlinedTextField(
+                value = email,
+                onValueChange = { email = it },
+                label = { Text(text = "Email") },
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email),
+                singleLine = true,
+                modifier = Modifier.fillMaxWidth(),
+                colors = TextFieldDefaults.outlinedTextFieldColors(
+                    focusedBorderColor = Color.Black,
+                    unfocusedBorderColor = Color.Black
+                ),
+                shape = MaterialTheme.shapes.medium,
+                textStyle = TextStyle(fontSize = 16.sp),
+                maxLines = 1
+            )
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            OutlinedTextField(
+                value = password,
+                onValueChange = { password = it },
+                label = { Text(text = "Password") },
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+                visualTransformation = if (passwordVisible) VisualTransformation.None else PasswordVisualTransformation(),
+                singleLine = true,
+                trailingIcon = {
+                    val image = if (passwordVisible) Icons.Filled.Visibility else Icons.Filled.VisibilityOff
+                    IconButton(onClick = { passwordVisible = !passwordVisible }) {
+                        Icon(imageVector = image, contentDescription = "Toggle password visibility")
                     }
-                    launchSingleTop = true
-                }
-            }
-        ) {
-            Text(
-                text = "New User? Create Your Account",
-                style = TextStyle(fontWeight = FontWeight.Bold, fontSize = 16.sp)
+                },
+                modifier = Modifier.fillMaxWidth(),
+                colors = TextFieldDefaults.outlinedTextFieldColors(
+                    focusedBorderColor = Color.Black,
+                    unfocusedBorderColor = Color.Black
+                ),
+                shape = MaterialTheme.shapes.medium,
+                textStyle = TextStyle(fontSize = 16.sp),
+                maxLines = 1
             )
+
+            Spacer(modifier = Modifier.height(30.dp))
+
+            var buttonPressed by remember { mutableStateOf(false) }
+
+            AnimatedVisibility(
+                visible = buttonPressed,
+                enter = scaleIn(animationSpec = tween(300))
+            ) {
+                Text("Processing...", style = TextStyle(fontWeight = FontWeight.Bold, fontSize = 18.sp))
+            }
+
+            ElevatedButton(
+                onClick = {
+                    if (email.isEmpty() || password.isEmpty()) {
+                        Toast.makeText(context, "Please provide all fields", Toast.LENGTH_SHORT).show()
+                    } else {
+                        buttonPressed = true
+                        navHostController.navigate(Routes.BottomNav.routes) {
+                            popUpTo(Routes.Login.routes) { inclusive = true }
+                            launchSingleTop = true
+                        }
+                    }
+                },
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Text(
+                    text = "Login",
+                    style = TextStyle(fontWeight = FontWeight.ExtraBold, fontSize = 20.sp),
+                    modifier = Modifier.padding(vertical = 6.dp)
+                )
+            }
+
+            Spacer(modifier = Modifier.height(20.dp))
+
+            TextButton(
+                onClick = {
+                    navHostController.navigate(Routes.Register.routes) {
+                        popUpTo(navHostController.graph.startDestinationId) {
+                            inclusive = true
+                        }
+                        launchSingleTop = true
+                    }
+                }
+            ) {
+                Text(
+                    text = "New User? Create Your Account",
+                    style = TextStyle(fontWeight = FontWeight.ExtraBold, fontSize = 16.sp)
+                )
+            }
         }
     }
 }
--- app/src/main/java/com/example/jobmatchai/Screens/Register.kt
+++ app/src/main/java/com/example/jobmatchai/Screens/Register.kt
@@ -1,13 +1,171 @@
 package com.example.jobmatchai.Screens
 
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import android.widget.Toast
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Visibility
+import androidx.compose.material.icons.filled.VisibilityOff
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
+import com.example.jobmatchai.Navigation.Routes
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun Register(navHostController: NavHostController) {
+    var name by remember { mutableStateOf("") }
+    var email by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+    var phone by remember { mutableStateOf("") }
+    var isVisible by remember { mutableStateOf(false) }
+    var passwordVisible by remember { mutableStateOf(false) }
+    val context = LocalContext.current
 
-    Text("Register Screen")
+    LaunchedEffect(Unit) {
+        isVisible = true
+    }
 
+    AnimatedVisibility(visible = isVisible, enter = androidx.compose.animation.fadeIn(animationSpec = tween(1000))) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(Color(0xFFFFCDD2))
+                .padding(16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+
+            Text(
+                text = "Register",
+                style = TextStyle(fontWeight = FontWeight.ExtraBold, fontSize = 24.sp)
+            )
+
+            Spacer(modifier = Modifier.height(50.dp))
+
+            OutlinedTextField(
+                value = name,
+                onValueChange = { name = it },
+                label = { Text("Name") },
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Text),
+                modifier = Modifier.fillMaxWidth(),
+                singleLine = true,
+                colors = TextFieldDefaults.outlinedTextFieldColors(
+                    focusedBorderColor = Color.Black,
+                    unfocusedBorderColor = Color.Black
+                ),
+                shape = MaterialTheme.shapes.medium,
+                textStyle = TextStyle(fontSize = 16.sp)
+            )
+
+            Spacer(modifier = Modifier.height(8.dp))
+
+            OutlinedTextField(
+                value = email,
+                onValueChange = { email = it },
+                label = { Text("Email") },
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email),
+                modifier = Modifier.fillMaxWidth(),
+                singleLine = true,
+                colors = TextFieldDefaults.outlinedTextFieldColors(
+                    focusedBorderColor = Color.Black,
+                    unfocusedBorderColor = Color.Black
+                ),
+                shape = MaterialTheme.shapes.medium,
+                textStyle = TextStyle(fontSize = 16.sp)
+            )
+
+            Spacer(modifier = Modifier.height(8.dp))
+
+
+            OutlinedTextField(
+                value = password,
+                onValueChange = { password = it },
+                label = { Text(text = "Password") },
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+                visualTransformation = if (passwordVisible) VisualTransformation.None else PasswordVisualTransformation(),
+                singleLine = true,
+                trailingIcon = {
+                    val image = if (passwordVisible) Icons.Filled.Visibility else Icons.Filled.VisibilityOff
+                    IconButton(onClick = { passwordVisible = !passwordVisible }) {
+                        Icon(imageVector = image, contentDescription = "Toggle password visibility")
+                    }
+                },
+                modifier = Modifier.fillMaxWidth(),
+                colors = TextFieldDefaults.outlinedTextFieldColors(
+                    focusedBorderColor = Color.Black,
+                    unfocusedBorderColor = Color.Black
+                ),
+                shape = MaterialTheme.shapes.medium,
+                textStyle = TextStyle(fontSize = 16.sp),
+                maxLines = 1
+            )
+
+            Spacer(modifier = Modifier.height(8.dp))
+
+            OutlinedTextField(
+                value = phone,
+                onValueChange = { phone = it },
+                label = { Text("Phone Number") },
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Phone),
+                modifier = Modifier.fillMaxWidth(),
+                singleLine = true,
+                colors = TextFieldDefaults.outlinedTextFieldColors(
+                    focusedBorderColor = Color.Black,
+                    unfocusedBorderColor = Color.Black
+                ),
+                shape = MaterialTheme.shapes.medium,
+                textStyle = TextStyle(fontSize = 16.sp)
+            )
+
+            Spacer(modifier = Modifier.height(30.dp))
+
+            Button(
+                onClick = {
+                    if (email.isEmpty() || password.isEmpty() || name.isEmpty() || phone.isEmpty()) {
+                        Toast.makeText(context, "Please provide all fields", Toast.LENGTH_SHORT).show()
+                    } else {
+                        navHostController.navigate(Routes.BottomNav.routes) {
+                            popUpTo(Routes.Login.routes) { inclusive = true }
+                            launchSingleTop = true
+                        }
+                    }
+                },
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Text("Submit")
+            }
+            Spacer(modifier = Modifier.height(20.dp))
+
+            TextButton(
+                onClick = {
+                    navHostController.navigate(Routes.Login.routes) {
+                        popUpTo(navHostController.graph.startDestinationId) {
+                            inclusive = true
+                        }
+                        launchSingleTop = true
+                    }
+                }
+            ) {
+                Text(
+                    text = "Already have an Account",
+                    style = TextStyle(fontWeight = FontWeight.ExtraBold, fontSize = 20.sp)
+                )
+            }
+        }
+    }
 }
